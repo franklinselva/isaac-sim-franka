9dde5250afab3354dc6d8c7ccd582d44c7afbc80
franklinselva <franklinselva10@gmail.com>
2024-07-05 15:58:19 +0200

    Add franka's panda urdf and simplify description package

---
+++ ros_ws/src/franka_demo/launch/joint_state_publisher.launch.py
@@ -0,0 +1,95 @@
+#  Copyright (c) 2023 Franka Robotics GmbH
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+
+import xacro
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+
+from launch import LaunchContext, LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+
+
+def robot_state_publisher_spawner(context: LaunchContext, load_gripper, ee_id):
+    load_gripper_str = context.perform_substitution(load_gripper)
+    ee_id_str = context.perform_substitution(ee_id)
+    franka_xacro_filepath = os.path.join(
+        get_package_share_directory("franka_demo"),
+        "urdf/robot/",
+        "panda/panda.urdf.xacro",
+    )
+    robot_description = xacro.process_file(
+        franka_xacro_filepath, mappings={"hand": load_gripper_str, "ee_id": ee_id_str}
+    ).toprettyxml(indent="  ")
+
+    return [
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            output="screen",
+            parameters=[{"robot_description": robot_description}],
+        )
+    ]
+
+
+def generate_launch_description():
+    load_gripper_parameter_name = "load_gripper"
+    load_gripper = LaunchConfiguration(load_gripper_parameter_name)
+
+    ee_id_parameter_name = "ee_id"
+    ee_id = LaunchConfiguration(ee_id_parameter_name)
+
+    rviz_file = os.path.join(
+        get_package_share_directory("franka_description"),
+        "rviz",
+        "visualize_franka.rviz",
+    )
+
+    robot_state_publisher_spawner_opaque_function = OpaqueFunction(
+        function=robot_state_publisher_spawner, args=[load_gripper, ee_id]
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                load_gripper_parameter_name,
+                default_value="true",
+                description="Use end-effector if true. Default value is franka hand. "
+                "Robot is loaded without end-effector otherwise",
+            ),
+
+            DeclareLaunchArgument(
+                ee_id_parameter_name,
+                default_value="franka_hand",
+                description="ID of the type of end-effector used. Supporter values: "
+                "none, franka_hand, cobot_pump",
+            ),
+            robot_state_publisher_spawner_opaque_function,
+            Node(
+                package="joint_state_publisher_gui",
+                executable="joint_state_publisher_gui",
+                name="joint_state_publisher_gui",
+                remappings=[("/joint_states", "/isaac_joint_commands")],
+            ),
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                arguments=["--display-config", rviz_file],
+            ),
+        ]
+    )
